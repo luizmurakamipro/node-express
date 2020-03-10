f55ce330865fae150f45f1d9a29a9621320a0736
Luiz Murakami <luizhm.murakami@univem.edu.br>
2020-03-10 20:12:27 -0300

    Mongoose installed

--- src/server.js
+++ src/server.js
@@ -13,14 +13,19 @@ const PORT = process.env.port || 3000;
 // Definindo as rotas
 const router = express.Router(); // Interceptar todas as rotas
 
-// Middleware
+// Intecerpt by Middleware
 router.use((req, res, next) => {
     console.log("Intercept by Middleware");
     next(); // Continua para próxima API
 });
 
+// Rota padrão
 router.get('/', (req, res) => {
     res.json({"message":"Route test OK"});
+});
+
+router.get('/about', (req, res) => {
+    res.json({"message":"About test OK!"});
 });
 
 // Caminho padrão para APIs

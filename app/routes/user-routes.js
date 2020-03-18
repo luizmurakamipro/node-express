79a4d01eefab045ac74480e8d191158b2ec350aa
Luiz Murakami <luizhm.murakami@univem.edu.br>
2020-03-17 22:21:49 -0300

    Relacionamento de usuario com produto feito, produto é salvo no usuario e usuario é salvo no produto

--- app/routes/user-routes.js
+++ app/routes/user-routes.js
@@ -8,57 +8,52 @@ const OK_FLAG = 200;
 const BADREQUEST_FLAG = 400;
 
 // Inserir usuario.
-router
-    .post('/', (req, res) => {
-        try {
-            var usuario = new Usuario();
+router.post('/', (req, res) => {
+    var usuario = new Usuario();
 
-            usuario.nome = req.body.nome;
-            usuario.cpf = req.body.cpf;
+    usuario.nome = req.body.nome;
+    usuario.cpf = req.body.cpf;
 
-            usuario.save(error => {
-                if (error)
-                    res.status(BADREQUEST_FLAG).send({error: "Erro ao tentar salvar usuario"});
+    usuario.save(error => {
+        if (error)
+            res.status(BADREQUEST_FLAG).send({error: "Erro ao tentar salvar usuario"});
                 
-                res.status(CREATE_FLAG).send({message: "Usuario inserido com sucesso"});
-            });
-        } catch (err) {
-            res.status(BADREQUEST_FLAG).send({error: "Informações incorretas"});
-        }
+        res.status(CREATE_FLAG).send({message: "Usuario inserido com sucesso"});
     });
+});
 
 // Listar usuarios inseridos
-router
-    .get('/', (req, res) => {
-        Usuario.find((err, users) => {
-            if (err)
-                res.send(err);
+router.get('/', (req, res) => {
+    Usuario.find((err, users) => {
+        if (err)
+            res.send(err);
 
-            res.status(OK_FLAG).json({
-                message:"Usuarios encontrados!",
-                usuarios: users
-            });
+        res.status(OK_FLAG).json({
+            message:"Usuarios encontrados!",
+             usuarios: users
         });
     });
+});
 
 
-router.
-    post('/:userId/products', (req, res) => { 
-        const { userId } = req.params;
-        const novoProduto = new Produto(req.body);
-        var id = new mongoose.Types.ObjectId(userId);
-        const usuario = Usuario.findOne(id);
+router.post('/:userId/addproducts', async (req, res) => { 
+    const { userId } = req.params;
+    const novoProduto = new Produto(req.body);
+    const usuario = await Usuario.findById(userId);
 
-        novoProduto.comprador = usuario;
-        novoProduto.save();
-
-        usuario.produtos.push(novoProduto);
-        usuario.save(error => {
-            if (error)
-                res.status(BADREQUEST_FLAG).send({message: "Erro ao inserir produto no usuario"});
-
-            res.status(CREATE_FLAG).send({message: "Produto inserido no usuario com sucesso"});
-        });
+    novoProduto.comprador = usuario;
+    novoProduto.save(error => {
+        if (error)
+            res.status(BADREQUEST_FLAG).send({message: "Erro ao salvar produto com usuario inserido"});
     });
+
+    usuario.produtos.push(novoProduto);
+    usuario.save(error => {
+        if (error)
+            res.status(BADREQUEST_FLAG).send({message: "Erro ao inserir produto no usuario"});
+
+        res.status(CREATE_FLAG).send({message: "Produto inserido no usuario com sucesso"});
+    });
+});
 
 module.exports = router;
